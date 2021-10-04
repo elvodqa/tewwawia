9d4800f76d78d2129089a6775aa0555296b8c566
Emir <emircengiz21@gmail.com>
2021-10-04 13:39:46 +0300

    HitCircleStack

--- core/src/com/elvodqa/gameplay/HitCircleStack.java
+++ core/src/com/elvodqa/gameplay/HitCircleStack.java
@@ -1,5 +1,47 @@
 package com.elvodqa.gameplay;
 
-public class HitCircleStack {
+import java.util.Dictionary;
+import java.util.Enumeration;
 
+public class HitCircleStack extends Dictionary {
+
+
+    public HitCircleStack() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Enumeration keys() {
+        return null;
+    }
+
+    @Override
+    public Enumeration elements() {
+        return null;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
 }
