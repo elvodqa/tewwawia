c8b36878c6c58360fecf9ed47825d4ba792bff0c
Emir <emircengiz21@gmail.com>
2021-10-04 12:43:17 +0300

    added playfield class

---
+++ core/src/com/elvodqa/gameplay/PlayArea.java
@@ -0,0 +1,32 @@
+package com.elvodqa.gameplay;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.awt.*;
+
+public class PlayArea {
+    public final int width = 640;
+    public final int height = 480;
+    private ShapeRenderer shapeRenderer;
+
+    public PlayArea() {
+        shapeRenderer = new ShapeRenderer();
+    }
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        int x = (Gdx.graphics.getWidth()) / 2 - (320);
+        int y = (Gdx.graphics.getHeight()) / 2 - (240);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled); //I'm using the Filled ShapeType, but remember you have three of them
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.rect(x,y,width,height); //assuming you have created those x, y, width and height variables
+        shapeRenderer.end();
+
+    }
+
+}
--- core/src/com/elvodqa/tewwawia/EditorState.java
+++ core/src/com/elvodqa/tewwawia/EditorState.java
@@ -1,17 +1,23 @@
 package com.elvodqa.tewwawia;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.elvodqa.gameplay.PlayArea;
 
 public class EditorState implements Screen {
     tewwawiaGame game;
     BitmapFont font = new BitmapFont();
     SpriteBatch batch;
+    PlayArea playArea;
 
     public EditorState(tewwawiaGame game) {
         this.game = game;
         batch = new SpriteBatch();
+        playArea = new PlayArea();
+        Gdx.graphics.setWindowedMode(800, 600);
+
     }
 
     @Override
@@ -21,9 +27,12 @@ public class EditorState implements Screen {
 
     @Override
     public void render(float delta) {
+        playArea.render();
         batch.begin();
+
         font.draw(batch, "state: editor", 20, 20);
         batch.end();
+
     }
 
     @Override
